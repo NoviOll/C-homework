7cafd9479814d5b4fb52bb042c39fcb59d3add78
novioll <novioll@mail.ru>
2022-08-08 22:18:35 +0300

    дз 1.1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int Number1 = 5;
+int Number2 = 7;
+{
+    if(Number2 > Number1)
+    {
+        Console.WriteLine("Большее число " + Number2);
+        Console.WriteLine("Меньшее число " + Number1);
+    }
+    else {
+        Console.WriteLine("Большее число + Number1");
+        Console.WriteLine("Меньшее число + Number2");
+    }
+}
